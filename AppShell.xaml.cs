2dd5bba399175345be518a89289ceb92b58ab8e0
Vinicius F. Murcia <vini.fernandes.murcia@gmail.com>
2026-04-05 22:46:22 -0300

    Projeto Previsão do Tempo

---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Previsao_Tempo;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Previsao_Tempo;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
